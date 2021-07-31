31366c26346fb023be198dec22f31135b40a07bf
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-31 17:13:07 -0300

    :tada: initial structure

---
+++ bin/alura_bank.dart
@@ -0,0 +1,5 @@
+import 'package:alura_bank/alura_bank.dart' as alura_bank;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alura_bank.calculate()}!');
+}
---
+++ test/alura_bank_test.dart
@@ -0,0 +1,8 @@
+import 'package:alura_bank/alura_bank.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
